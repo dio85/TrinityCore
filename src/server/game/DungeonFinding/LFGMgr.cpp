dc6322c552ea58d45b36f564641996361acf7378
dio85 <k1ng@freemail.hu>
2024-10-29 10:35:35 +0100

    Core/LFGMgr fix warrning2

--- src/server/game/DungeonFinding/LFGMgr.cpp
+++ src/server/game/DungeonFinding/LFGMgr.cpp
@@ -52,11 +52,11 @@ LFGDungeonData::LFGDungeonData(): id(0), name(), map(0), type(0), subType(0), ex
 }
 
 LFGDungeonData::LFGDungeonData(LFGDungeonsEntry const* dbc) : id(dbc->ID), name(dbc->Name[sWorld->GetDefaultDbcLocale()]), map(dbc->MapID),
-        type(uint8(dbc->TypeID)), subType(0), expansion(uint8(dbc->ExpansionLevel)), group(uint8(dbc->GroupID)),
-        contentTuningId(uint32(dbc->ContentTuningID)), RequiredPlayerConditionId(uint32(dbc->RequiredPlayerConditionId)),
-        difficulty(Difficulty(dbc->DifficultyID)), seasonal((dbc->Flags[0] & LFG_FLAG_SEASONAL) != 0),
-        x(0.0f), y(0.0f), z(0.0f), o(0.0f),
-        requiredItemLevel(0), requiredChromieTime(0), finalDungeonEncounterId(0)
+    type(uint8(dbc->TypeID)), subType(0), expansion(uint8(dbc->ExpansionLevel)),
+    group(uint8(dbc->GroupID)), contentTuningId(uint32(dbc->ContentTuningID)),
+    difficulty(Difficulty(dbc->DifficultyID)), RequiredPlayerConditionId(uint32(dbc->RequiredPlayerConditionId)),
+    seasonal((dbc->Flags[0] & LFG_FLAG_SEASONAL) != 0), x(0.0f), y(0.0f), z(0.0f), o(0.0f),
+    requiredItemLevel(0), requiredChromieTime(0), finalDungeonEncounterId(0)
 {
 }
 
