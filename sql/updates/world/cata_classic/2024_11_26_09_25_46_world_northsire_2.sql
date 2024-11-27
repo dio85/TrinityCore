1d414ebe26bb73c68e9f42f372d15135c9edb37a
dio85 <k1ng@freemail.hu>
2024-11-27 10:28:56 +0100

    Revert "DB/Northshire_quests_update"
    This reverts commit f475c28853b9cbbe5d8915c8262d6a83b32c8b9d.

--- sql/updates/world/cata_classic/2024_11_26_09_25_46_world_northsire_2.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM `creature_queststarter` WHERE (`id`=197 AND `quest`=29078) OR (`id`=197 AND `quest`=31139) OR (`id`=197 AND `quest`=31140) OR (`id`=197 AND `quest`=31143) ;
-INSERT INTO `creature_queststarter` (`id`, `quest`, `VerifiedBuild`) VALUES
-(197, 28768, 57564),
-(197, 3101, 57564),
-(197, 29080, 57564);
