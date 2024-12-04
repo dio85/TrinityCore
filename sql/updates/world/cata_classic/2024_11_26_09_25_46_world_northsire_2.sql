46a74234c82c98c6698ebd3a62364fc9049b3db1
dio85 <k1ng@freemail.hu>
2024-12-04 08:36:25 +0100

    DB/FIX_STARTUP_DUPLICATE_ERRORS

--- sql/updates/world/cata_classic/2024_11_26_09_25_46_world_northsire_2.sql
+++ sql/updates/world/cata_classic/2024_11_26_09_25_46_world_northsire_2.sql
@@ -1,4 +1,4 @@
-DELETE FROM `creature_queststarter` WHERE (`id`=197 AND `quest`=29078) OR (`id`=197 AND `quest`=31139) OR (`id`=197 AND `quest`=31140) OR (`id`=197 AND `quest`=31143) ;
+DELETE FROM `creature_queststarter` WHERE (`id`=197 AND `quest`=29078) OR (`id`=197 AND `quest`=28768) OR (`id`=197 AND `quest`=3101) OR (`id`=197 AND `quest`=29080) OR (`id`=197 AND `quest`=31139) OR (`id`=197 AND `quest`=31140) OR (`id`=197 AND `quest`=31143) ;
 INSERT INTO `creature_queststarter` (`id`, `quest`, `VerifiedBuild`) VALUES
 (197, 28768, 57564),
 (197, 3101, 57564),
